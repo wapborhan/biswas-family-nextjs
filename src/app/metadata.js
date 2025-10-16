45d983cb9f5f075b803d04527364e61423d06e9c
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2025-10-16 21:15:18 +0600

    Meta Add

---
+++ src/app/metadata.js
@@ -0,0 +1,35 @@
+// app/metadata.js
+
+export const siteMetadata = {
+  title: "Biswas Family",
+  description:
+    "Official website of the Biswas Family – celebrating tradition, culture, and heritage.",
+  keywords: [
+    "Biswas Family",
+    "Family Heritage",
+    "Bangladesh",
+    "Tradition",
+    "Culture",
+    "Family Tree",
+  ],
+  authors: [{ name: "Biswas Family" }],
+  creator: "Biswas Family",
+  publisher: "Biswas Family",
+  openGraph: {
+    title: "Biswas Family",
+    description:
+      "Official website of the Biswas Family – celebrating tradition, culture, and heritage.",
+    url: "https://family.wapborhan.com/",
+    siteName: "Biswas Family",
+    images: [
+      {
+        url: "screenshot.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Biswas Family",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
